203c24bd28f373468223847f702f1628aa4b93d8
Peng Chao <1377318286@qq.com>
2024-03-12 19:46:13 +0800

    coments for server demo

--- scripts/EMAGE_2024/demo.py
+++ scripts/EMAGE_2024/demo.py
@@ -597,5 +597,8 @@ demo = gr.Interface(
 if __name__ == "__main__":
     os.environ["MASTER_ADDR"]='127.0.0.1'
     os.environ["MASTER_PORT"]='8675'
+    #os.environ["PYOPENGL_PLATFORM"]='egl'
+    #os.environ["EGL_DEVICE_ID"]='1'
     #os.environ["TORCH_DISTRIBUTED_DEBUG"] = "DETAIL"
     demo.launch(share=True)
+    #demo.launch(share=True, server_name='0.0.0.0', server_port=8501)
